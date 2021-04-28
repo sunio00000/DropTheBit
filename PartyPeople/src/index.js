8df9fbed80b1ad120b19cdb66d0b362886e8a273
Taeyang Oh <sunio00000@naver.com>
2021-04-28 23:50:11 +0900

    refactor: Exercise react-redux - 리덕스가 무엇인가. - 컨테이너, 리듀서, 액션, 뷰컴퍼넌트의 개념 익히

--- PartyPeople/src/index.js
+++ PartyPeople/src/index.js
@@ -4,11 +4,21 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import GameManager from './GameManager';
 import LoadingScreen from './LoadingScreen';
+import Counter from './components/Counter';
+
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Provider } from 'react-redux';
+import CounterContainer from './containers/CounterContainer';
+
+const store = createStore(reducers);
 
 ReactDOM.render(
     <div className='GameManager'>
         <div className='main'>
-             <GameManager/>
+            <Provider store = {store}>
+             <CounterContainer/>
+            </Provider>
         </div>
     </div>,
     document.getElementById('root')
